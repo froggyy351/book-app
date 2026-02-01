ae7d8d124a3d907f34076e354ee526943e0abc4b
sakoichi <kouichi19980817@gmail.com>
2026-02-01 16:23:05 +0900

    feat: database connection

--- src/main/java/com/bookmanager/App.java
+++ src/main/java/com/bookmanager/App.java
@@ -1,13 +1,31 @@
 package com.bookmanager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static final String URL = "jdbc:h2:./.bookdb";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+    public static void main( String[] args ){
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement stmt = conn.createStatement()){
+                    String sql = "CREATE TABLE IF NOT EXISTS books ("
+                                + "id INT AUTO_INCREMENT PRIMARY KEY"
+                                + "title VARCHAR(255) NOT NULL"
+                                + "author VARCHAR(255))";
+
+                    stmt.execute(sql);
+                    System.out.println("データベース接続に成功しました。");
+            } catch (SQLException e) {
+                System.err.println("データベース関連例外が発生しました。");
+                e.getStackTrace();
+            }
     }
 }
